c27ca69499086e502c573f0ae83b55bd5fa244b7
Man <Man@DESKTOP-JAGCICS>
2026-01-16 21:55:03 +0500

    Добавьте файлы проекта.

---
+++ FirstCPlusPlusWindow/FirstCPlusPlusWindow.cpp
@@ -0,0 +1,48 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <tlhelp32.h>
+
+LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
+WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure);
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
+
+    WNDCLASS SoftwareMainClass = NewWindowClass((HBRUSH)COLOR_WINDOW, LoadCursor(NULL, IDC_ARROW), hInst, LoadIcon(NULL, IDI_QUESTION), L"MainWndClass", SoftwareMainProcedure);
+
+    if (!RegisterClassW(&SoftwareMainClass)) { return -1; }
+
+    MSG SoftwareMainMessage = { 0 };
+
+    CreateWindow(L"MainWndClass", L"First c++window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 800, 600, NULL, NULL, NULL, NULL);
+
+    while (GetMessage(&SoftwareMainMessage, NULL, NULL, NULL)) {
+        TranslateMessage(&SoftwareMainMessage);
+        DispatchMessage(&SoftwareMainMessage);
+    }
+    return 0;
+}
+
+WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure) {
+
+    WNDCLASS NWC = { 0 };
+
+    NWC.hbrBackground = BGColor;
+    NWC.hIcon = Icon;
+    NWC.hInstance = hInst;
+    NWC.lpszClassName = Name;
+    NWC.hCursor = Cursor;
+    NWC.lpfnWndProc = Procedure;
+
+    return NWC;
+}
+
+LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+    switch (msg) {
+    case WM_CREATE:
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default: return DefWindowProc(hWnd, msg, wp, lp);
+    }
+}
