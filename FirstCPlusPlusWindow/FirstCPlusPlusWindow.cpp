ec994c612f39d3a8c7aa2b20c54664eaf26a0a39
Ivan_Shutov <soulerfun@gmail.com>
2026-01-17 10:53:28 +0500

    Добавление базовых GUI вещей: статичное поле, поле для ввода текста, кнопка

--- FirstCPlusPlusWindow/FirstCPlusPlusWindow.cpp
+++ FirstCPlusPlusWindow/FirstCPlusPlusWindow.cpp
@@ -1,9 +1,6 @@
 ﻿#include <windows.h>
 #include "Header.h"
 
-LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
-WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure);
-
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
 
     WNDCLASS SoftwareMainClass = NewWindowClass((HBRUSH)COLOR_WINDOW, LoadCursor(NULL, IDC_ARROW), hInst, LoadIcon(NULL, IDI_QUESTION), L"MainWndClass", SoftwareMainProcedure);
@@ -12,7 +9,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
 
     MSG SoftwareMainMessage = { 0 };
 
-    CreateWindow(L"MainWndClass", L"First c++window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 800, 600, NULL, NULL, NULL, NULL);
+    CreateWindow(L"MainWndClass", L"First c++window", WS_VISIBLE | WS_SYSMENU | WS_MINIMIZEBOX, 100, 100, 400, 300, NULL, NULL, NULL, NULL);
 
     while (GetMessage(&SoftwareMainMessage, NULL, NULL, NULL)) {
         TranslateMessage(&SoftwareMainMessage);
@@ -21,6 +18,8 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
     return 0;
 }
 
+
+// функция создания нового окна
 WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure) {
 
     WNDCLASS NWC = { 0 };
@@ -35,10 +34,17 @@ WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON I
     return NWC;
 }
 
+// обработка событий
 LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
     switch (msg) {
     case WM_COMMAND:
         switch (wp) {
+        case OnButtonClicked:
+            MessageBoxA(hWnd, "Button is clicked!", "Thank you for click :)", MB_OK);
+            break;
+        case OnClearField:
+            SetWindowTextA(hEditControl, ""); // очищает окно клсса edit
+            break;
         case OnExitSoftware:
             PostQuitMessage(0);
             break;
@@ -53,8 +59,10 @@ LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp
             break;
         default: break;
         }
+        break;
     case WM_CREATE:
         MainWndAddMenus(hWnd);
+        MainWndAddWidgets(hWnd);
         break;
     case WM_DESTROY:
         PostQuitMessage(0);
@@ -63,6 +71,7 @@ LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp
     }
 }
 
+// реализация функции добавление верхнего меню
 void MainWndAddMenus(HWND hWnd) {
     HMENU RootMenu = CreateMenu();
     HMENU SubMenu = CreateMenu();
@@ -70,16 +79,31 @@ void MainWndAddMenus(HWND hWnd) {
 
     AppendMenu(SubMenu, MF_POPUP, (UINT_PTR)SubActionMenu, L"Action");
 
-    AppendMenu(SubActionMenu, MF_STRING, OnMenuAction1, L"Menu 1");
-    AppendMenu(SubActionMenu, MF_SEPARATOR, NULL, NULL);
-
-    AppendMenu(SubActionMenu, MF_STRING, OnMenuAction2, L"Menu 2");
-    AppendMenu(SubActionMenu, MF_SEPARATOR, NULL, NULL);
-
-    AppendMenu(SubActionMenu, MF_STRING, OnMenuAction3, L"Menu 3");
-
     AppendMenu(RootMenu, MF_POPUP, (UINT_PTR)SubMenu, L"File");
+    AppendMenu(RootMenu, MF_POPUP, (UINT_PTR)SubMenu, L"Help");
     AppendMenu(RootMenu, MF_STRING, OnExitSoftware, L"Exit");
 
     SetMenu(hWnd, RootMenu);
+}
+
+//реализация функции добавления виджетов
+
+void MainWndAddWidgets(HWND hWnd) {
+
+    // параметры :
+    // 1. имя класса (static - позволяет вывести текст) (edit - позволяет вводить текст) (button - кнопка)
+    // 2. текст виджета
+    // 3. стиль окна
+    // 4 и 5. расположение верхнего левого угла виджета в окне
+    // 6 и 7. ширина и высота виджета в пикселях
+    // 8. задаётся главное окно 
+    // есть еще параметры, но их рассмотрим потом
+    CreateWindowA("button", "Clear", WS_VISIBLE | WS_CHILD | ES_CENTER, 5, 5, 120, 30, hWnd, HMENU(OnClearField), NULL, NULL);
+
+    CreateWindowA("static", "STATUS: Hello window!", WS_VISIBLE | WS_CHILD , 200, 5, 490, 20, hWnd, NULL, NULL, NULL);
+
+    hEditControl = CreateWindowA("edit", "This is EDIT control", WS_VISIBLE | WS_CHILD| ES_MULTILINE | WS_VSCROLL, 5, 50, 380, 80, hWnd, NULL, NULL, NULL);
+
+    CreateWindowA("button", "Click me!", WS_VISIBLE | WS_CHILD | ES_CENTER, 5, 140, 120, 40, hWnd, HMENU(OnButtonClicked), NULL, NULL);
+    // функция CreateWindowA возвращает созданное ей окно
 }
--- FirstCPlusPlusWindow/Header.h
+++ FirstCPlusPlusWindow/Header.h
@@ -3,9 +3,16 @@
 #define OnMenuAction2		2
 #define OnMenuAction3		3
 #define OnExitSoftware		4
+#define OnClearField		5
+#define OnButtonClicked		6
 
+HWND hEditControl;
 
 LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
 WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure);
 
+//функция для создания меню
 void MainWndAddMenus(HWND hWnd);
+
+//функция для создания виджетов
+void MainWndAddWidgets(HWND hWnd);
